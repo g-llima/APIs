dbfce5ae7412ec02de26f8f8e72a97bf02390364
Gabriel Lima <78111347+g-llima@users.noreply.github.com>
2022-07-06 09:48:31 -0300

    GEATS_API: Desenvolvido model do produto

---
+++ GEATS_API/geatsapi/src/main/java/com/gabriel/Models/Product.java
@@ -0,0 +1,38 @@
+package main.java.com.gabriel.Models;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.gabriel.geatsapi.Converters.ListToStringConverter;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String product_name;
+    private String description;
+
+    @Column
+    @Convert(converter = ListToStringConverter.class)
+    private List<String> tags;
+
+    private String calories;
+    private boolean veg;
+    private String img_url;
+    private BigDecimal price;
+    private int quantity;
+}
