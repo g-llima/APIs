1ac39227827ae9d894598792ad27ebf8d5543695
g-llima <gabrielrtxon@gmail.com>
2022-07-24 16:57:00 -0300

    Redit Clone API: Arquivos iniciais

---
+++ reddit-clone-api/src/test/java/com/gabriel/redditcloneapi/RedditCloneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.redditcloneapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
